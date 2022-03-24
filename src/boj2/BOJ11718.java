1c901a9e60e5b9ba2b8ea9a553e1d4456b8653bf
leetingman <dwa08@naver.com>
2022-03-24 13:52:43 +0900

    Thu Mar 24 boj String

---
+++ src/boj2/BOJ11718.java
@@ -0,0 +1,15 @@
+package boj2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        String sb;
+        while((sb=br.readLine())!=null){
+            System.out.println(sb);
+        }
+    }
+}
