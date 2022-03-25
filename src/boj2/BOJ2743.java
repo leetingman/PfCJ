951889585a48f2ce058307c6cbbb9718276b9d05
leetingman <dwa08@naver.com>
2022-03-25 13:37:18 +0900

    Fri Mar 25 boj String

---
+++ src/boj2/BOJ2743.java
@@ -0,0 +1,14 @@
+package boj2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        String str=br.readLine();
+        System.out.print(str.length());
+    }
+
+}
